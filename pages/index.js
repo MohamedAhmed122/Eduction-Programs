5cc8831587abcbdf2bc73fe867e55c67cda4595b
Mohamed Desouky <mohamed.ahm9833@gmail.com>
2021-02-19 12:35:11 +0700

    Create the Login Page and the Global CSS

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+
+import Image from 'next/image'
+import classNames from 'classnames'
+import styles from '../styles/index.module.css'
+import FromInput from '../Components/Form/FormInput'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import {FormControlLabel, Checkbox} from '@material-ui/core'
+
+
+const validationSchema = Yup.object({
+  email: Yup.string().required().email(),
+  password: Yup.string().required().min(5),
+});
+
 
 export default function Home() {
+
+  const route = useRouter()
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className='flexAll'>
+        <div  className={classNames(styles.loginSvgContainer, 'flex_center')}>
+            <Image height={500} width={500} src='/assets/login.svg' alt='Login image' />
         </div>
-      </main>
+        <div className={classNames(styles.loginForm, 'flex_col')}>
+            <h1 className='main_title'>Welcome to Website name  </h1>
+            <Formik 
+              validationSchema={validationSchema}
+              initialValues={{email: '', password:''}}
+              onSubmit={(values)=> console.log(values)}
+            >
+            {({ dirty,isSubmitting, isValid })=>(
+              <Form style={{width: '70%'}}>
+                <FromInput label='Email' name='email' />
+                <FromInput type="password" label='Password' name='password' />
+                <FormControlLabel
+                    control={<Checkbox name="checkedB" color="primary"/>}
+                    label="remember me"
+                />
+                <div className='flex_center' style={{marginTop: '3rem'}}>
+                  <button style={{width: '70%'}} type='submit' className='btn_primary'> Login</button>
+                </div>
+                <h4 className='text_align'>you don't have account! 
+                    <span onClick={()=> route.push('/register')} className='span_color'>Register</span>
+                </h4>
+              </Form>
+            )}
+            </Formik>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        </div>
     </div>
   )
 }
+
